a0e25c5282dce1607e81d2823c72cdf110454656
Nandhu A Regi <nandhuaregi1996@gmail.com>
2022-09-05 15:50:52 +0530

    added home component

--- showbiz-ui/src/app/app.module.ts
+++ showbiz-ui/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {MatTabsModule} from '@angular/material/tabs';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
